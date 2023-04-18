e34110f9b96e1c84cfe6a71aa09da45d1a118be7
Eskil Brännerud <eskil.brann@gmail.com>
2023-04-18 14:16:04 +0200

    first commit

---
+++ scrape.py
@@ -0,0 +1,64 @@
+import praw
+import pandas as pd
+
+
+global badWordsSet
+global subredditsList
+
+def createArrays():
+    global badWordsSet
+    global subredditsList
+    badWordsText = open("./badWords.txt", "r")
+    badWordsSet = set(badWordsText.read().split('\n'))
+
+    subredditsText = open("./subreddits.txt", "r")
+    subredditsList = subredditsText.read().split('\n')
+    
+    
+    
+def search(subredditArray, bad_words, reddit_read_only) -> int:
+
+    result = {}
+    
+    for subredditName in subredditArray:
+        counter = 0
+        subreddit = reddit_read_only.subreddit(subredditName)    
+        for post in subreddit.top(time_filter="year", limit=100):
+            postArray = post.selftext.split()
+            for word in postArray:
+                if word in bad_words:
+                    counter += 1
+                    break    
+        result[subredditName]=counter        
+    return result
+    
+
+reddit_read_only = praw.Reddit(client_id="jHaMZuAi8kiEX9erg6yH_w",         # your client id
+                               client_secret="QS6aK093s1gRBjt6XvW6RQ4dSXaySA",      # your client secret
+                               user_agent="True")        # your user agent
+ 
+ 
+
+
+
+
+
+createArrays()
+
+print(search(subredditsList, badWordsSet, reddit_read_only))
+
+print(badWordsSet)
+
+
+""""
+subreddit = reddit_read_only.subreddit("redditdev")
+ 
+# Display the name of the Subreddit
+print("Display Name:", subreddit.display_name)
+ 
+# Display the title of the Subreddit
+print("Title:", subreddit.title)
+ 
+# Display the description of the Subreddit
+print("Description:", subreddit.description)
+"""""
